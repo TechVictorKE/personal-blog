cb163cae12daa6673a5be807563ae77aed273376
TechVictorKE <victor.maina@student.moringaschool.com>
2021-06-21 19:16:28 +0300

    Modified db_uri

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringa:moringa@localhost/flaskblog"
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:moringa@localhost/flaskblog'
     UPLOADED_PHOTOS_DEST = "app/static/photos"
 
     # email configurations
@@ -14,7 +14,9 @@ class Config:
 
 class ProdConfig(Config):
     
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','')
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', "postgresql://", 1)
 
 class TestConfig(Config):
     
